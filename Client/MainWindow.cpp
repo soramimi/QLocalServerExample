8f0d46f84540082ed9c14ba6a10dd7b14c031384
soramimi <fi7s-fct@asahi-net.or.jp>
2017-04-12 23:16:43 +0900

    test

--- Client/MainWindow.cpp
+++ Client/MainWindow.cpp
@@ -1,7 +1,6 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
 #include <QLocalSocket>
-#include <QThread>
 
 MainWindow::MainWindow(QWidget *parent) :
 	QMainWindow(parent),
@@ -19,12 +18,19 @@ void MainWindow::on_pushButton_clicked()
 {
 	QLocalSocket *sock = new QLocalSocket;
 	connect(sock, &QLocalSocket::connected, [&](){
+#if 1
 		sock->write("Hell", 4);
 		sock->waitForBytesWritten();
 		sock->write("o, w", 4);
 		sock->waitForBytesWritten();
 		sock->write("orld", 4);
 		sock->waitForBytesWritten();
+		sock->write("!\n", 2);
+		sock->waitForBytesWritten();
+#else
+		sock->write("Hello, world!\n", 14);
+		sock->waitForBytesWritten();
+#endif
 		sock->disconnectFromServer();
 		delete sock;
 	});
